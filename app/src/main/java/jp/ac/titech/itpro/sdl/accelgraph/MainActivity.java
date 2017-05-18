7aa52bd90cb9e8d54b6c1f50f64b6ec85805c603
MasakazuOhmura <tourokusaito.masakazu@gmail.com>
2017-05-18 21:36:11 +0900

    add magnetic field

--- app/src/main/java/jp/ac/titech/itpro/sdl/accelgraph/MainActivity.java
+++ app/src/main/java/jp/ac/titech/itpro/sdl/accelgraph/MainActivity.java
@@ -15,13 +15,37 @@ import java.util.Locale;
 
 public class MainActivity extends Activity implements SensorEventListener {
 
+    /**
+     * 行列数
+     */
+    private static final int MATRIX_SIZE = 16;
+    /**
+     * 三次元(XYZ)
+     */
+    private static final int DIMENSION = 3;
+    /**
+     * 地磁気行列
+     */
+    private float[] mMagneticValues;
+    /**
+     * 加速度行列
+     */
+    private float[] mAccelerometerValues;
+    /**
+     * 回転角度
+     */
+    private int pitchX;
+    private int rollY;
+    private int azimuthZ;
+
     private final static String TAG = "MainActivity";
 
     private TextView rateView, accuracyView;
-    private GraphView xView, yView, zView;
+    private GraphView xView, yView, zView, xView2, yView2, zView2;
 
     private SensorManager sensorMgr;
     private Sensor accelerometer;
+    private Sensor magneticField;
 
     private final static long GRAPH_REFRESH_WAIT_MS = 20;
 
@@ -46,9 +70,14 @@ public class MainActivity extends Activity implements SensorEventListener {
         xView = (GraphView) findViewById(R.id.x_view);
         yView = (GraphView) findViewById(R.id.y_view);
         zView = (GraphView) findViewById(R.id.z_view);
+        xView2 = (GraphView) findViewById(R.id.x_view2);
+        yView2 = (GraphView) findViewById(R.id.y_view2);
+        zView2 = (GraphView) findViewById(R.id.z_view2);
 
         sensorMgr = (SensorManager) getSystemService(SENSOR_SERVICE);
         accelerometer = sensorMgr.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        magneticField = sensorMgr.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+
         if (accelerometer == null) {
             Toast.makeText(this, getString(R.string.toast_no_accel_error),
                     Toast.LENGTH_SHORT).show();
@@ -64,6 +93,7 @@ public class MainActivity extends Activity implements SensorEventListener {
         super.onResume();
         Log.i(TAG, "onResume");
         sensorMgr.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_FASTEST);
+        sensorMgr.registerListener(this, magneticField, SensorManager.SENSOR_DELAY_FASTEST);
         th = new GraphRefreshThread();
         th.start();
     }
@@ -78,11 +108,43 @@ public class MainActivity extends Activity implements SensorEventListener {
 
     @Override
     public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD)
+            mMagneticValues = event.values.clone();
+        else if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER)
+            mAccelerometerValues = event.values.clone();
+
+        if (mMagneticValues != null && mAccelerometerValues != null) {
+            float[] rotationMatrix = new float[MATRIX_SIZE];
+            float[] inclinationMatrix = new float[MATRIX_SIZE];
+            float[] remapedMatrix = new float[MATRIX_SIZE];
+            float[] orientationValues = new float[DIMENSION];
+            // 加速度センサーと地磁気センサーから回転行列を取得
+            SensorManager.getRotationMatrix(rotationMatrix, inclinationMatrix, mAccelerometerValues, mMagneticValues);
+            SensorManager.remapCoordinateSystem(rotationMatrix, SensorManager.AXIS_X, SensorManager.AXIS_Z, remapedMatrix);
+            SensorManager.getOrientation(remapedMatrix, orientationValues);
+            // ラジアン値を変換し、それぞれの回転角度を取得する
+            azimuthZ = radianToDegrees(orientationValues[0]);
+            pitchX = radianToDegrees(orientationValues[1]);
+            rollY = radianToDegrees(orientationValues[2]);
+            Log.e(TAG, "X=" + pitchX + " Y=" + rollY + " Z=" + azimuthZ);
+        }
+
         vx = alpha * vx + (1 - alpha) * event.values[0];
         vy = alpha * vy + (1 - alpha) * event.values[1];
         vz = alpha * vz + (1 - alpha) * event.values[2];
+        Log.e(TAG, "X=" + vx + " Y=" + vy + " Z=" + vz);
         rate = ((float) (event.timestamp - prevts)) / (1000 * 1000);
         prevts = event.timestamp;
+    }
+
+    /**
+     * ラジアンを角度に変換する
+     *
+     * @param angrad ラジアン
+     * @return 角度
+     */
+    private int radianToDegrees(float angrad) {
+        return (int) Math.floor(angrad >= 0 ? Math.toDegrees(angrad) : 360 + Math.toDegrees(angrad));
     }
 
     @Override
@@ -102,12 +164,15 @@ public class MainActivity extends Activity implements SensorEventListener {
                             xView.addData(vx, true);
                             yView.addData(vy, true);
                             zView.addData(vz, true);
+
+                            xView2.addData(pitchX, true);
+                            yView2.addData(rollY, true);
+                            zView2.addData(azimuthZ, true);
                         }
                     });
                     Thread.sleep(GRAPH_REFRESH_WAIT_MS);
                 }
-            }
-            catch (InterruptedException e) {
+            } catch (InterruptedException e) {
                 Log.e(TAG, e.toString());
                 th = null;
             }
